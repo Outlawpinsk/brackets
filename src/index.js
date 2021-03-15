4399a837cfd3317647702180f216c7ffa05e59fd
outlawpinsk <outlawpinsk@gmail.com>
2021-03-16 00:39:45 +0300

    feat: complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let bracketsConfigMap = new Map(bracketsConfig);
+    let currentOpenBracket = [];
+  
+    for (let symbol of str) {
+      if (currentOpenBracket.length === 0) {
+               if (bracketsConfigMap.has(symbol)) {
+          currentOpenBracket.push(symbol);
+        } else {
+          return false;
+        }
+      } else {
+  
+        if (symbol === bracketsConfigMap.get(currentOpenBracket[currentOpenBracket.length - 1])) {
+          currentOpenBracket.pop();
+        } else if (bracketsConfigMap.has(symbol)) {
+          currentOpenBracket.push(symbol);
+        } else {
+          return false;
+        }
+      }
+    }
+    if (currentOpenBracket.length !== 0) {
+      return false;
+    }
+    return true;
+  }
